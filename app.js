f8093758b449a18979febad054341476cbd3d7dc
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2024-02-21 17:10:33 -1200

    Primer cambio

---
+++ app.js
@@ -0,0 +1,21 @@
+
+var myNumero=122;
+
+
+//PALABRAS CLAVES LET, VAR, CONST (VARIABLES)
+var myVariable = 8;
+
+let number ;
+number = 10;
+
+myNumero = 250;
+console.log(myNumero);
+
+
+
+number = 22;
+console.log(number);
+//UNA LINEA DE COMENTARIO
+
+/* COMENTARIOS VARIAS LINEAS
+*/
